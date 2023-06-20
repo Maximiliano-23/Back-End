e4bb6504a241f3f26a08460b1076581b40451073
Maximiliano Pereira <maxidape44@gmail.com>
2023-06-20 15:02:45 -0300

    Dockerfile

---
+++ Dockerfile
@@ -0,0 +1,6 @@
+FROM amazoncorretto:17-alpine-jdk
+MAINTAINER Maximiliano_Pereira
+
+COPY target/Maximiliano_Pereira-0.0.1-SNAPSHOT.jar Maximiliano_Pereira_app.jar
+
+ENTRYPOINT ["java","-jar","/Maximiliano_Pereira_app.jar"]
--- src/main/java/com/portfolio/Maximiliano_Pereira/Controller/CEducacion.java
+++ src/main/java/com/portfolio/Maximiliano_Pereira/Controller/CEducacion.java
@@ -29,7 +29,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @author GUSTAVO
  */
 @RestController
-@RequestMapping
+@RequestMapping ("/educacion")
 @CrossOrigin(origins = "http://localhost:4200")
 public class CEducacion {
     @Autowired
