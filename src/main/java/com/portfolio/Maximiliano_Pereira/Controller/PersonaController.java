f49dbd068b9ade44eb6515c88cc57bba79782dff
Maximiliano Pereira <maxidape44@gmail.com>
2023-06-24 18:45:33 -0300

    Correcion Server

--- src/main/java/com/portfolio/Maximiliano_Pereira/Controller/PersonaController.java
+++ src/main/java/com/portfolio/Maximiliano_Pereira/Controller/PersonaController.java
@@ -25,7 +25,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @author GUSTAVO
  */
 @RestController
-@CrossOrigin(origins = "http://localhost:4200/")
+@CrossOrigin(origins = "https://frontend-maximilianopereira.web.app/")
 public class PersonaController {
     @Autowired IPersonaService ipersonaService;
     
